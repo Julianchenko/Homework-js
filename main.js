a2ec5fc0b8c769a3da41fb30ef01a629aa851dad
Julianchenko <95035985+Julianchenko@users.noreply.github.com>
2021-12-07 19:34:22 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,87 @@
+//let
+
+
+let str1 = 'Кропивницкий';
+console.log(str1);
+
+let str2 = 'Украина';
+console.log(str2);
+
+let number = 227000;
+console.log (number);
+
+//boolean
+let variable = true;
+console.log (variable);
+
+
+
+//Задание 2
+
+const number1 = 40;
+const number2 = 70;
+
+
+let result = number1*number2
+console.log(result);
+
+
+//Задание 3
+
+const time = 2;
+const speedOfFirst = 95;
+const speedOfSecond = 114;
+
+let result = (speedOfFirst+speedOfSecond)*2
+console.log(result);
+
+
+
+let result = 
+console.log(result);
+
+//Задание 4
+
+if ('randomNumber<20') {
+    console.log ('randomNumber меньше 20');
+    
+} else if ('randomNumber>50') {
+    console.log ('randomNumber больше 50');
+
+} else {console.log ('randomNumber больше 20, и меньше 50');
+
+}
+
+
+
+//Задание 5
+
+// swich case
+
+switch (Number) {
+    case 'randomNumber<20' 
+        console.log ('randomNumber меньше 20');
+        break;
+    case 'randomNumber>50'  
+        console.log ('randomNumber больше 50');
+        break;
+
+    default 
+        ('randomNumber больше 20, и меньше 50');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
